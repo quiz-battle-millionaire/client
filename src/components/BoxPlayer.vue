07bf2197912752ce86922504145bf5f22c8e90b2
robbybenta <robbybenta7@gmail.com>
2021-03-18 21:47:10 +0700

    Styling Component

---
+++ src/components/BoxPlayer.vue
@@ -0,0 +1,22 @@
+<template>
+     <div class="pt-5 pb-3">
+            <div class="box-score col-6 mx-auto  positon-absolute">
+            <div class="row my-2">
+                <div class="col-4 text-center mt-5 py-3"><h3 class="my-3">Player</h3><h4 class="my-0 py-0">indro</h4>
+                <h4 class="my-0 py-0">indro</h4></div>
+                <div class="col-4 text-center mt-2"><h2>Playerlist</h2></div>
+                <div class="col-4 text-center mt-5 py-3"><h3 class="my-3">Score</h3><h4 class="my-0 py-0">90</h4>
+                <h4 class="my-0 py-0">90</h4></div>
+            </div>
+        </div>
+        </div>
+</template>
+<style scoped>
+ .box-score{
+        border-radius: 10px;
+        background-color: white;
+        z-index: 10;
+        height: 20vh;
+        overflow: scroll;
+    }
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="container-fluid page-login mx-0 px-0">
+      <div
+        class="col-12 position-absolute d-flex align-content-center flex-wrap my-auto"
+        style="z-index: 10; height: 100vh"
+      >
+      
+        <div class="d-flex justify-content-center position-absolute">
+          <div class="title position-absolute"><h1 class="text-white font-weight-bold">Game PS5</h1>
+          </div>
+          <img class="w-img" src="../../public/img/Group 51.png" alt=""></div>
+        <div class="box-login col-12 col-md-4 mx-auto px-5 py-5">
+          
+          <form>
+            <div class="form-group mt-5">
+              <label for="text"> <h2>Nama Lo Boy</h2></label>
+              <input type="text" class="form-control" v-model="name" placeholder="Masukkin sini boy" />
+            </div>
+            <button type="submit" @click="setName" class="btn btn-fun mt-4 col-12 text-white">
+             <h2>Masuk Boy</h2>
+            </button>
+          </form>
+        </div>
+      </div>
+      <div class="overlay-image px-0 mx-0"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      name:''
+    }
+  },
+  methods:{
+    setName(){
+      localStorage.setItem('name',this.name)
+      this.$router.push('/Home')
+    }
+  }
+}
+</script>
+<style scoped>
+.w-img{
+  margin-top:100px ;
+  z-index: 100;
+  width: 15%;
+}
+.title{
+ z-index: 101;
+}
+.btn-fun{
+  background-color: #023e8a;
+}
+</style>
