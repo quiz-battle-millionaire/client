9a7fdbb6dfdd7aff71596a2140f5f359417582e2
robbybenta <robbybenta7@gmail.com>
2021-03-19 11:54:35 +0700

    repairing bug win

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,5 @@
 import data from "./data"
+import router from "@/router/index"
 
 export function questions ({ commit }) {
   commit('SET_QUESTIONS',data)
@@ -12,4 +13,10 @@ export function SOCKET_player ({ commit }, payload) {
   export function SOCKET_winner ({ commit }, payload) {
     commit('SET_WINNER', payload)
     console.log(payload)
-    } 
+    this._vm.$swal(
+      `Game sudah selesai Dan pemenangnya adalah ${payload.name} dengan Score ${payload.score}`
+    ).then(()=>{
+      localStorage.removeItem('name')
+      router.push('/')
+    })
+    }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,109 +1,112 @@
 <template>
-    <div class="themeColor">
-
-        <div class="container-fluid page-home mx-0 px-0">
-
-        
-            <div>
-                <img class="presenting-img" src="../../public/img/willsmith.png" alt="">
+  <div class="themeColor">
+    <div class="container-fluid page-home mx-0 px-0">
+      <div>
+        <img
+          class="presenting-img"
+          src="../../public/img/willsmith.png"
+          alt=""
+        />
+      </div>
+      <boxplayer />
+      <div class="question col-8 mt-0 mx-auto">
+        <div class="pt-1">
+          <div class="box-question mt-0 py-3 px-3">
+            <h1>{{ question[increment].question }}</h1>
+          </div>
+          <div class="answer row mt-5">
+            <div
+              class="col-6 my-2 d-flex justify-content-center"
+              v-for="(answer, i) in question[increment].option"
+              :key="i"
+            >
+              <button
+                type="button"
+                class="btn btn-warning text-center"
+                @click="sendAnswer(answer.value, question[increment].id)"
+              >
+                <h1 class="text-center">
+                  {{ answer.list }}
+                </h1>
+              </button>
             </div>
-        <boxplayer/>
-        <div class="question col-8 mt-0 mx-auto">
-            <div class="pt-1">
-                <div class="box-question mt-0 py-3 px-3">
-                <h1>{{question[increment].question}}</h1>
-            </div>
-           <div class="answer row mt-5">
-               <div class="col-6 my-2 d-flex justify-content-center" v-for="(answer,i) in question[increment].option" :key="i">
-                
-                       <button type="button" class="btn btn-warning text-center" @click="sendAnswer(answer.value,question[increment].id )">
-                              <h1 class="text-center">
-                           {{answer.list}}
-  </h1>
-                       </button>
-                     
-                   </div>
-                  <button type="button" class="btn btn-danger col-8 mx-auto mt-5" @click="logout">Keluar ae boy</button>
-           </div>
-            
-            </div>
+            <button
+              type="button"
+              class="btn btn-danger col-8 mx-auto mt-5"
+              @click="logout"
+            >
+              Keluar ae boy
+            </button>
+          </div>
         </div>
-       </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import boxplayer from "@/components/BoxPlayer"
+import boxplayer from '@/components/BoxPlayer'
 export default {
-    data(){
-        return{
-            increment:0
-        }
-    },
-    methods:{
-         showAlert(payload) {
-      // Use sweetalert2
-      this.$swal(payload);
-    },
-        logout(){
-            
-            this.$router.push('/')
-            this.$socket.emit('logout',localStorage.name)
-            localStorage.removeItem('name')
-        },
-        sendAnswer(list,id){
-
-         console.log(list,id)
-            let data =this.$store.getters.questions
-            let dataAnswer = data.filter(data => data.id === id);
-            console.log(dataAnswer[0])
-            if(list===dataAnswer[0].answer){
-            this.$socket.emit('setScore',localStorage.name,10)
-            if(this.increment<2){
-            this.showAlert('Jawaban anda benar')
-            }
-            else if(this.increment===2){
- this.showAlert(`Game sudah selesai Dan pemenangnya adalah ${winner.name} dengan Score ${winner.score}`)
-            }
-            }
-            else{
-                 this.$socket.emit('setScore',localStorage.name,-10)
-              this.showAlert('Jawaban anda salah')
-            }
-            this.increment++
-             if(this.increment===2){
-            this.increment=0
-            this.$socket.emit('setWin','Selesai')
-            this.checkWin()
-        }
-        }
-    },
-    components:{
-        boxplayer
-    },
-    created(){
-        this.$store.dispatch('questions')
-        this.$socket.emit('setPlayer', {
-            name:localStorage.name,
-            score:0
-        })
-        
-    },
-    computed:{
-        question(){
-            console.log(this.$store.getters.questions)
-           return this.$store.getters.questions
-        },
-        player(){
-            return this.$store.getters.player
-        },
-          winner(){
-            return this.$store.state.winner
-        }
+  data () {
+    return {
+      increment: 0
     }
+  },
+  methods: {
+    showAlert (payload) {
+      // Use sweetalert2
+      this.$swal(payload)
+    },
+    logout () {
+      this.$router.push('/')
+      this.$socket.emit('logout', localStorage.name)
+      localStorage.removeItem('name')
+    },
+    sendAnswer (list, id) {
+      console.log(list, id)
+      const data = this.$store.getters.questions
+      const dataAnswer = data.filter(data => data.id === id)
+      console.log(dataAnswer[0])
+      if (list === dataAnswer[0].answer) {
+        this.$socket.emit('setScore', localStorage.name, 10)
+         this.showAlert('Jawaban anda benar')
+      } else {
+        this.$socket.emit('setScore', localStorage.name, -10)
+        this.showAlert('Jawaban anda salah')
+      }
+      
+      if (this.increment === 2) {
+        this.increment = 0
+        this.$socket.emit('setWin', 'Selesai')
+      }
+      this.increment++
+    }
+  },
+  components: {
+    boxplayer
+  },
+  created () {
+    this.$store.dispatch('questions')
+    this.$socket.emit('setPlayer', {
+      name: localStorage.name,
+      score: 0
+    })
+  },
+  computed: {
+    question () {
+      console.log(this.$store.getters.questions)
+      return this.$store.getters.questions
+    },
+    player () {
+      return this.$store.getters.player
+    },
+    winner () {
+      return this.$store.state.winner
+    }
+  }
 }
 
-//ini kalo mau dipake lagi pindahin ke css
+// ini kalo mau dipake lagi pindahin ke css
 //   .themeColor{
 //         background: #1a2a6c;  /* fallback for old browsers */
 //         background: -webkit-linear-gradient(to bottom, #fdbb2d, #b21f1f, #1a2a6c);  /* Chrome 10-25, Safari 5.1-6 */
@@ -113,21 +116,21 @@ export default {
 //     }
 </script>
 <style scoped>
-    .question{
-        height: 50vh;
-        background-color:rgb(202, 203, 224);
-        border-radius: 7px;
-    }
-    .box-question{
-        background-color: rgb(96, 84, 201);
-        border-radius: 7px;
-    }
-   .presenting-img {
-        position: absolute;
-        left: 0%;
-        margin-left: 0;
-        margin-top: 55vh;
-        width: 400px;
-        height: auto;
+.question {
+  height: 50vh;
+  background-color: rgb(202, 203, 224);
+  border-radius: 7px;
+}
+.box-question {
+  background-color: rgb(96, 84, 201);
+  border-radius: 7px;
+}
+.presenting-img {
+  position: absolute;
+  left: 0%;
+  margin-left: 0;
+  margin-top: 55vh;
+  width: 400px;
+  height: auto;
 }
 </style>
